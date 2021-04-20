06edaa84d6c2008236d918ffbc5d12e75c327c52
marokazu <ksg.inu.1118@gmail.com>
2021-04-20 11:36:34 +0900

    first commit

---
+++ 10.回帰分析/10th.py
@@ -0,0 +1,222 @@
+#In[1]
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+import numpy as np
+
+#In[2]
+rng = np.random.RandomState(1)
+x = 10 * rng.rand(50)
+y = 2 * x - 5 + rng.randn(50)
+plt.scatter(x, y);
+plt.show()
+
+#In[3]
+from sklearn.linear_model import LinearRegression
+model = LinearRegression(fit_intercept=True)
+
+model.fit(x[:, np.newaxis], y)
+
+xfit = np.linspace(0, 10, 1000)
+yfit = model.predict(xfit[:, np.newaxis])
+
+plt.scatter(x, y)
+plt.plot(xfit, yfit);
+plt.show()
+
+#In[4]
+print("Model slope:    ", model.coef_[0])
+print("Model intercept:", model.intercept_)
+
+#In[5]
+rng = np.random.RandomState(1)
+X = 10 * rng.rand(100, 3)
+y = 0.5 + np.dot(X, [1.5, -2., 1.])
+
+model.fit(X, y)
+print(model.intercept_)
+print(model.coef_)
+
+#In[6]
+from sklearn.preprocessing import PolynomialFeatures
+x = np.array([2, 3, 4])
+poly = PolynomialFeatures(3, include_bias=False)
+poly.fit_transform(x[:, None])
+
+#In[7]
+from sklearn.pipeline import make_pipeline
+poly_model = make_pipeline(PolynomialFeatures(7),LinearRegression())
+
+#In[8]
+rng = np.random.RandomState(1)
+x = 10 * rng.rand(50)
+y = np.sin(x) + 0.1 * rng.randn(50)
+
+poly_model.fit(x[:, np.newaxis], y)
+yfit = poly_model.predict(xfit[:, np.newaxis])
+
+plt.scatter(x, y)
+plt.plot(xfit, yfit);
+plt.show()
+
+#In[9]
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class GaussianFeatures(BaseEstimator, TransformerMixin):
+    """Uniformly spaced Gaussian features for one-dimensional input"""
+    
+    def __init__(self, N, width_factor=2.0):
+        self.N = N
+        self.width_factor = width_factor
+    
+    @staticmethod
+    def _gauss_basis(x, y, width, axis=None):
+        arg = (x - y) / width
+        return np.exp(-0.5 * np.sum(arg ** 2, axis))
+        
+    def fit(self, X, y=None):
+        # create N centers spread along the data range
+        self.centers_ = np.linspace(X.min(), X.max(), self.N)
+        self.width_ = self.width_factor * (self.centers_[1] - self.centers_[0])
+        return self
+        
+    def transform(self, X):
+        return self._gauss_basis(X[:, :, np.newaxis], self.centers_,
+                                 self.width_, axis=1)
+    
+gauss_model = make_pipeline(GaussianFeatures(20),
+                            LinearRegression())
+gauss_model.fit(x[:, np.newaxis], y)
+yfit = gauss_model.predict(xfit[:, np.newaxis])
+
+plt.scatter(x, y)
+plt.plot(xfit, yfit)
+plt.xlim(0, 10);
+plt.show()
+
+#In[10]
+model = make_pipeline(GaussianFeatures(30),
+                      LinearRegression())
+model.fit(x[:, np.newaxis], y)
+
+plt.scatter(x, y)
+plt.plot(xfit, model.predict(xfit[:, np.newaxis]))
+
+plt.xlim(0, 10)
+plt.ylim(-1.5, 1.5);
+plt.show()
+
+#In[11]
+def basis_plot(model, title=None):
+    fig, ax = plt.subplots(2, sharex=True)
+    model.fit(x[:, np.newaxis], y)
+    ax[0].scatter(x, y)
+    ax[0].plot(xfit, model.predict(xfit[:, np.newaxis]))
+    ax[0].set(xlabel='x', ylabel='y', ylim=(-1.5, 1.5))
+    
+    if title:
+        ax[0].set_title(title)
+
+    ax[1].plot(model.steps[0][1].centers_,
+               model.steps[1][1].coef_)
+    ax[1].set(xlabel='basis location',
+              ylabel='coefficient',
+              xlim=(0, 10))
+    
+model = make_pipeline(GaussianFeatures(30), LinearRegression())
+basis_plot(model)
+
+#In[12]
+from sklearn.linear_model import Ridge
+model = make_pipeline(GaussianFeatures(30), Ridge(alpha=0.1))
+basis_plot(model, title='Ridge Regression')
+
+#In[13]
+from sklearn.linear_model import Lasso
+model = make_pipeline(GaussianFeatures(30), Lasso(alpha=0.001))
+basis_plot(model, title='Lasso Regression')
+
+#In[14]
+# !curl -o FremontBridge.csv https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD
+
+#In[15]
+import pandas as pd
+counts = pd.read_csv('FremontBridge.csv', index_col='Date', parse_dates=True)
+weather = pd.read_csv('data/BicycleWeather.csv', index_col='DATE', parse_dates=True)
+
+#In[16]
+daily = counts.resample('d').sum()
+daily['Total'] = daily.sum(axis=1)
+daily = daily[['Total']] # remove other columns
+
+#In[17]
+days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+for i in range(7):
+    daily[days[i]] = (daily.index.dayofweek == i).astype(float)
+
+#In[18]
+from pandas.tseries.holiday import USFederalHolidayCalendar
+cal = USFederalHolidayCalendar()
+holidays = cal.holidays('2012', '2016')
+daily = daily.join(pd.Series(1, index=holidays, name='holiday'))
+daily['holiday'].fillna(0, inplace=True)
+
+#In[19]
+def hours_of_daylight(date, axis=23.44, latitude=47.61):
+    """Compute the hours of daylight for the given date"""
+    days = (date - pd.datetime(2000, 12, 21)).days
+    m = (1. - np.tan(np.radians(latitude))
+         * np.tan(np.radians(axis) * np.cos(days * 2 * np.pi / 365.25)))
+    return 24. * np.degrees(np.arccos(1 - np.clip(m, 0, 2))) / 180.
+
+daily['daylight_hrs'] = list(map(hours_of_daylight, daily.index))
+daily[['daylight_hrs']].plot()
+plt.ylim(8, 17)
+plt.show()
+
+#In[20]
+# temperatures are in 1/10 deg C; convert to C
+weather['TMIN'] /= 10
+weather['TMAX'] /= 10
+weather['Temp (C)'] = 0.5 * (weather['TMIN'] + weather['TMAX'])
+
+# precip is in 1/10 mm; convert to inches
+weather['PRCP'] /= 254
+weather['dry day'] = (weather['PRCP'] == 0).astype(int)
+
+daily = daily.join(weather[['PRCP', 'Temp (C)', 'dry day']])
+
+#In[21]
+daily['annual'] = (daily.index - daily.index[0]).days / 365.
+
+#In[22]
+daily.head()
+
+#In[23]
+# Drop any rows with null values
+daily.dropna(axis=0, how='any', inplace=True)
+
+column_names = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun', 'holiday',
+                'daylight_hrs', 'PRCP', 'dry day', 'Temp (C)', 'annual']
+X = daily[column_names]
+y = daily['Total']
+
+model = LinearRegression(fit_intercept=False)
+model.fit(X, y)
+daily['predicted'] = model.predict(X)
+
+#In[24]
+daily[['Total', 'predicted']].plot(alpha=0.5);
+plt.show()
+
+#In[25]
+params = pd.Series(model.coef_, index=X.columns)
+print(params)
+
+#In[26]
+from sklearn.utils import resample
+np.random.seed(1)
+err = np.std([model.fit(*resample(X, y)).coef_
+              for i in range(1000)], 0)
+
+#In[27]
+print(pd.DataFrame({'effect': params.round(0),'error': err.round(0)}))
---
+++ 11.SVM/11th.py
@@ -0,0 +1,225 @@
+#in[1]
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+# use seaborn plotting defaults
+import seaborn as sns; sns.set()
+
+#In[2]
+from sklearn.datasets.samples_generator import make_blobs
+X, y = make_blobs(n_samples=50, centers=2,
+                  random_state=0, cluster_std=0.60)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn');
+plt.show()
+
+#in[3]
+xfit = np.linspace(-1, 3.5)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plt.plot([0.6], [2.1], 'x', color='red', markeredgewidth=2, markersize=10)
+
+for m, b in [(1, 0.65), (0.5, 1.6), (-0.2, 2.9)]:
+    plt.plot(xfit, m * xfit + b, '-k')
+
+plt.xlim(-1, 3.5);
+plt.show()
+
+#In[4]
+xfit = np.linspace(-1, 3.5)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+
+for m, b, d in [(1, 0.65, 0.33), (0.5, 1.6, 0.55), (-0.2, 2.9, 0.2)]:
+    yfit = m * xfit + b
+    plt.plot(xfit, yfit, '-k')
+    plt.fill_between(xfit, yfit - d, yfit + d, edgecolor='none',
+                     color='#AAAAAA', alpha=0.4)
+
+plt.xlim(-1, 3.5);
+plt.show()
+
+#In[5]
+from sklearn.svm import SVC # "Support vector classifier"
+model = SVC(kernel='linear', C=1E10)
+model.fit(X, y)
+
+#In[6]
+def plot_svc_decision_function(model, ax=None, plot_support=True):
+    """Plot the decision function for a 2D SVC"""
+    if ax is None:
+        ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    
+    # create grid to evaluate model
+    x = np.linspace(xlim[0], xlim[1], 30)
+    y = np.linspace(ylim[0], ylim[1], 30)
+    Y, X = np.meshgrid(y, x)
+    xy = np.vstack([X.ravel(), Y.ravel()]).T
+    P = model.decision_function(xy).reshape(X.shape)
+    
+    # plot decision boundary and margins
+    ax.contour(X, Y, P, colors='k',
+               levels=[-1, 0, 1], alpha=0.5,
+               linestyles=['--', '-', '--'])
+    
+    # plot support vectors
+    if plot_support:
+        ax.scatter(model.support_vectors_[:, 0],
+                   model.support_vectors_[:, 1],
+                   s=300, linewidth=1, facecolors='none');
+    ax.set_xlim(xlim)
+    ax.set_ylim(ylim)
+
+#In[7]
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plot_svc_decision_function(model);
+plt.show()
+
+#In[8]
+print(model.support_vectors_)
+
+#In[9]
+def plot_svm(N=10, ax=None):
+    X, y = make_blobs(n_samples=200, centers=2, random_state=0, cluster_std=0.60)
+    X = X[:N]
+    y = y[:N]
+    model = SVC(kernel='linear', C=1E10)
+    model.fit(X, y)
+    
+    ax = ax or plt.gca()
+    ax.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+    ax.set_xlim(-1, 4)
+    ax.set_ylim(-1, 6)
+    plot_svc_decision_function(model, ax)
+
+fig, ax = plt.subplots(1, 2, figsize=(16, 6))
+fig.subplots_adjust(left=0.0625, right=0.95, wspace=0.1)
+for axi, N in zip(ax, [60, 120]):
+    plot_svm(N, axi)
+    axi.set_title('N = {0}'.format(N))
+plt.show()
+
+#In[10]
+from ipywidgets import interact, fixed
+interact(plot_svm, N=[10, 200], ax=fixed(None));
+plt.show()
+
+#In[11]
+from sklearn.datasets.samples_generator import make_circles
+X, y = make_circles(100, factor=.1, noise=.1)
+
+clf = SVC(kernel='linear').fit(X, y)
+
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plot_svc_decision_function(clf, plot_support=False);
+plt.show()
+
+#In[12]
+r = np.exp(-(X ** 2).sum(1))
+
+#In[13]
+from mpl_toolkits import mplot3d
+
+def plot_3D(elev=30, azim=30, X=X, y=y):
+    fig=plt.figure()
+    #ax = plt.subplot(projection='3d')
+    ax = fig.add_subplot(111, projection='3d')
+    ax.scatter3D(X[:, 0], X[:, 1], r, c=y, s=50, cmap='autumn')
+    ax.view_init(elev=elev, azim=azim)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    ax.set_zlabel('r')
+    fig.show()
+    
+interact(plot_3D, elev=[-90, 90], azip=(-180, 180), X=fixed(X), y=fixed(y));
+
+#In[14]
+clf = SVC(kernel='rbf', C=1E6)
+print(clf.fit(X, y))
+
+#In[15]
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plot_svc_decision_function(clf)
+plt.scatter(clf.support_vectors_[:, 0], clf.support_vectors_[:, 1], s=300, lw=1, facecolors='none');
+plt.show()
+
+#In[16]
+X, y = make_blobs(n_samples=100, centers=2, random_state=0, cluster_std=1.2)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn');
+plt.show()
+
+#In[17]
+X, y = make_blobs(n_samples=100, centers=2, random_state=0, cluster_std=0.8)
+
+fig, ax = plt.subplots(1, 2, figsize=(16, 6))
+fig.subplots_adjust(left=0.0625, right=0.95, wspace=0.1)
+
+for axi, C in zip(ax, [10.0, 0.1]):
+    model = SVC(kernel='linear', C=C).fit(X, y)
+    axi.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+    plot_svc_decision_function(model, axi)
+    axi.scatter(model.support_vectors_[:, 0], model.support_vectors_[:, 1], s=300, lw=1, facecolors='none');
+    axi.set_title('C = {0:.1f}'.format(C), size=14)
+plt.show()
+"""
+#In[18]
+from sklearn.datasets import fetch_lfw_people
+faces = fetch_lfw_people(min_faces_per_person=60)
+print(faces.target_names)
+print(faces.images.shape)
+
+#In[19]
+fig, ax = plt.subplots(3, 5)
+for i, axi in enumerate(ax.flat):
+    axi.imshow(faces.images[i], cmap='bone')
+    axi.set(xticks=[], yticks=[], xlabel=faces.target_names[faces.target[i]])
+plt.show()
+
+#In[20]
+from sklearn.svm import SVC
+from sklearn.decomposition import PCA as RandomizedPCA
+from sklearn.pipeline import make_pipeline
+
+pca = RandomizedPCA(n_components=150, whiten=True, random_state=42)
+svc = SVC(kernel='rbf', class_weight='balanced')
+model = make_pipeline(pca, svc)
+
+#In[21]
+from sklearn.model_selection import train_test_split
+Xtrain, Xtest, ytrain, ytest = train_test_split(faces.data, faces.target, random_state=42)
+
+#In[22]
+from sklearn.model_selection import GridSearchCV
+param_grid = {'svc__C': [1, 5, 10, 50], 'svc__gamma': [0.0001, 0.0005, 0.001, 0.005]}
+#grid = GridSearchCV(model, param_grid)
+grid = GridSearchCV(model,param_grid)
+#from sklearn import grid_search
+#grid = grid_search.GridSearchCV(RidgeRegression(),parameters,cv=5)
+#print(grid.best_params_)
+#print(grid)
+
+#In[23]
+model = grid.best_estimator_#ERROR
+yfit = model.predict(Xtest)
+
+#In[24]
+fig, ax = plt.subplots(4, 6)
+for i, axi in enumerate(ax.flat):
+    axi.imshow(Xtest[i].reshape(62, 47), cmap='bone')
+    axi.set(xticks=[], yticks=[])
+    axi.set_ylabel(faces.target_names[yfit[i]].split()[-1], color='black' if yfit[i] == ytest[i] else 'red')
+fig.suptitle('Predicted Names; Incorrect Labels in Red', size=14);
+
+#In[25]
+from sklearn.metrics import classification_report
+print(classification_report(ytest, yfit, target_names=faces.target_names))
+
+#In[26]
+from sklearn.metrics import confusion_matrix
+mat = confusion_matrix(ytest, yfit)
+sns.heatmap(mat.T, square=True, annot=True, fmt='d', cbar=False, xticklabels=faces.target_names, yticklabels=faces.target_names)
+plt.xlabel('true label')
+plt.ylabel('predicted label');
+plt.show()
+
+"""
