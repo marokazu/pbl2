237f43a86346048a9eaebfe86bc04d49f6957c5e
Ryo <ryo.matsuura.412@gmail.com>
2021-05-10 11:26:55 +0900

    音声データの追加、グラフ表示できるように改良

--- 3_Handling_data/信号データ/librosa_MFCC.py
+++ 3_Handling_data/信号データ/librosa_MFCC.py
@@ -1,12 +1,30 @@
 import librosa
-
+import librosa.display
+import numpy as np
+import matplotlib.pyplot as plt
 #ffmpegのインストールが必要
 
 a,sr = librosa.load("sample.mp3")
-
 data = []
 y = librosa.feature.mfcc(y=a, sr=sr)
 data.append(y)
 for i in range(len(data)):
     data[i] = sum(data[i])/len(data[i])
 
+
+fig = plt.figure()
+ax1 = fig.add_subplot(2, 2, 1)
+ax2 = fig.add_subplot(2, 2, 2)
+ax3 = fig.add_subplot(2, 2, 3)
+ax1.plot(data[0])
+ax2.plot(data[0])
+ax3.plot(data[0])
+ax1.set_xlim(150,770)
+ax2.set_xlim(2900,3750)
+ax3.set_xlim(3750,4700)
+
+ax1.set_title('150-770(midium)')
+ax2.set_title('2900-3750(low)')
+ax3.set_title('3750-4700(high)')
+fig.tight_layout()
+plt.show()
--- 3_Handling_data/信号データ/librosa_power.py
+++ 3_Handling_data/信号データ/librosa_power.py
@@ -1,12 +1,38 @@
 import librosa
 import numpy as np
+import matplotlib.pyplot as plt
 
 #ffmpegのインストールが必要
 
-a, sr = librosa.load("sample.mp3")
+def power_(n, sr, smp):
+    y = np.fft.fft(n)
+    amp = np.abs(y)
+    power = amp ** 2
+    x = np.linspace(0, sr, len(power))
+    freqs = np.fft.fftfreq(smp, 1/sr)
+    return power, freqs
 
-y = np.fft.fft(a)
-amp = np.abs(y)
-power = amp ** 2
-x = np.linspace(0, sr, len(power))
+a, sr_a = librosa.load("sample_1.mp3")
+b, sr_b = librosa.load("sample_2.mp3")
+c, sr_c = librosa.load("sample_3.mp3")
+p_a, f_a =power_(a, sr_a, 336960)
+p_b, f_b =power_(b, sr_b, 405504)
+p_c, f_c =power_(c, sr_c, 419904)
 
+fig = plt.figure()
+ax1 = fig.add_subplot(2, 2, 1)
+ax2 = fig.add_subplot(2, 2, 2)
+ax3 = fig.add_subplot(2, 2, 3)
+
+ax1.plot(f_a, p_a, color="blue", label="sample1")
+ax2.plot(f_b, p_b, color="green", label="sample2")
+ax3.plot(f_c, p_c, color="red", label="sample3")
+ax1.set_xlim(0,1000)
+ax2.set_xlim(0,1000)
+ax3.set_xlim(0,1000)
+ax1.set_title('sample_1(midium)')
+ax2.set_title('sample_2(low)')
+ax3.set_title('sample_3(high)')
+
+fig.tight_layout()
+plt.show()
